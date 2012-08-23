abae18203197f4536af345ea1f25c2a8fd515b08
guybedford <guybedford@Guys-MacBook-Air-2.local>
2012-08-23 15:13:18 +0200

    install script corrections

--- scripts/install.js
+++ scripts/install.js
@@ -1,8 +1,10 @@
 var fs = require('fs'),
   volo = require('volo');
 
+var basePath = __dirname + '/../../../';
+
 //load package.json
-var p = eval('(' + fs.readFileSync('package.json', 'utf-8') + ')');
+var p = eval('(' + fs.readFileSync(basePath + 'package.json', 'utf-8') + ')');
 
 //check amd.baseUrl, if not set to www/lib
 p.amd = p.amd || {};
@@ -15,7 +17,7 @@ p.volo.dependencies.require = 'github:jrburke/requirejs/2.0.6';
 p.volo.dependencies.zest = 'github:zestjs/zest/master';
 
 //save package.json
-fs.writeFileSync('package.json', JSON.stringify(p), 'utf-8');
+fs.writeFileSync(basePath + 'package.json', JSON.stringify(p, function(key, value) { return value; }, 2), 'utf-8');
 
 //run volo add
 volo(['add']);
